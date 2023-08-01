0863818601e63e8abd43e9625da53a0243e9c1dd
Pablo Valdes <am.pablo.vc@gmail.com>
2023-07-31 21:51:24 -0400

    - cleanup

--- amadeus-api/src/iosMain/kotlin/com/pablichj/incubator/amadeus/storage/DriverFactory.kt
+++ amadeus-api/src/iosMain/kotlin/com/pablichj/incubator/amadeus/storage/DriverFactory.kt
@@ -1,7 +1,6 @@
 package com.pablichj.incubator.amadeus.storage
 
 import app.cash.sqldelight.async.coroutines.synchronous
-import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.pablichj.incubator.amadeus.Database
--- iOSDemoApp/iOSDemoApp/iOSDemoAppApp.swift
+++ iOSDemoApp/iOSDemoApp/iOSDemoAppApp.swift
@@ -26,30 +26,29 @@ struct iOSDemoAppApp: App {
        
        var body: some Scene {
            WindowGroup {
-               ContentView(iosBridge: iosBridge)
-                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-                       print("application_willEnterForeground")
-                   }
-                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
-                       print("application_didBecomeActive")
-                       iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
-                           appLifecycleEvent: .start
-                       )
-                   }
-                   .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-                       print("application_willResignActive")
-                   }.onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
-                       print("application_didEnterBackground")
-                       iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
-                           appLifecycleEvent: .stop
-                       )
-                   }
+               ZStack {
+                   Color.white.ignoresSafeArea(.all) // status bar color
+                   ContentView(iosBridge: iosBridge)
+                       .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                           print("application_willEnterForeground")
+                       }
+                       .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                           print("application_didBecomeActive")
+                           iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
+                               appLifecycleEvent: .start
+                           )
+                       }
+                       .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                           print("application_willResignActive")
+                       }.onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification)) { _ in
+                           print("application_didEnterBackground")
+                           iosBridge.appLifecycleDispatcher.dispatchAppLifecycleEvent(
+                               appLifecycleEvent: .stop
+                           )
+                       }
+               }.preferredColorScheme(.light)
+               
            }
        }
 
-    /*var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }*/
 }
