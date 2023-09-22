c3a7af5f9cff0bfbd78c68d57f72257216cb30a7
Pablo Valdes <am.pablo.vc@gmail.com>
2023-09-22 11:26:41 -0400

    - Adds support to x64 arch

--- amadeus-api/build.gradle.kts
+++ amadeus-api/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "io.github.pablichjenkov"
-version = "0.3.0"
+version = "0.3.1"
 val mavenCentralUser = extra["mavenCentral.user"] as String
 val mavenCentralPass = extra["mavenCentral.pass"] as String
 
@@ -101,6 +101,7 @@ kotlin {
     }
 
     // IOS
+    iosX64()
     iosArm64()
     iosSimulatorArm64()
     /*ios {
@@ -165,10 +166,12 @@ kotlin {
         val androidInstrumentedTest by getting
 
         // IOS
+        val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
             dependencies {
@@ -176,10 +179,13 @@ kotlin {
                 implementation("app.cash.sqldelight:native-driver:2.0.0")
             }
         }
+
+        val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
